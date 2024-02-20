624d56cee350a247988c8c947a0298f023405764
Wahyu Setiawan <wahyu.creator911@gmail.com>
2024-02-20 13:53:48 +0700

    add enpoint post create article

--- database/mongorm.go
+++ database/mongorm.go
@@ -2,26 +2,42 @@ package mongorm
 
 import (
 	"context"
-	"fmt"
-	"log"
 
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func Connect(uri string) (*mongo.Client, error) {
-	clientOptions := options.Client().ApplyURI(uri)
-	client, err := mongo.Connect(context.Background(), clientOptions)
+type Article struct {
+	ID          primitive.ObjectID `bson:"_id,omitempty"`
+	Name        string             `bson:"name,omitempty"`
+	Permalink   string             `bson:"permalink,omitempty"`
+	Description string             `bson:"description,omitempty"`
+}
+
+type ArticleAdapter interface {
+	insertArticleIntoDatabase(article Article) *mongo.InsertOneResult
+}
+
+type articleAdapter struct {
+	article ArticleAdapter
+}
+
+func (W *articleAdapter) InsertArticle(article Article) {
+	W.article.insertArticleIntoDatabase(article)
+}
+
+type ArticleMongoDb struct {
+	client *mongo.Client
+}
+
+func (AM *ArticleMongoDb) insertArticleIntoDatabase(article Article) *mongo.InsertOneResult {
+	articleCollection := AM.client.Database("testing").Collection("article")
+
+	insertedData, err := articleCollection.InsertOne(context.TODO(), article)
 
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 
-	err = client.Ping(context.Background(), nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Successfully connected to MongoDB")
-	return client, err
+	return insertedData
 }
--- main.go
+++ main.go
@@ -1,30 +1,69 @@
 package main
 
 import (
-    "net/http"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 
-    "github.com/go-chi/chi/v5"
-    "github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-playground/validator/v10"
 )
 
-func getArticle(w http.ResponseWriter, r *http.Request){
+type requestArticle struct {
+	Name        string `validate:"required", json:"name"`
+	Description string `validate:"required", json:"description"`
+}
+
+func getArticle(w http.ResponseWriter, r *http.Request) {
 	// dateParam := chi.URLParam(r, "date")
 	// slugParam := chi.URLParam(r, "slug")
-
 
 	w.Write([]byte("data ini article"))
 }
 
+func createArticle(w http.ResponseWriter, r *http.Request) {
+	bodyData, _ := ioutil.ReadAll(r.Body)
+
+	var article requestArticle
+
+	json.Unmarshal(bodyData, &article)
+
+	fmt.Println(article)
+
+	validate := validator.New()
+
+	err := validate.Struct(article)
+
+	if err != nil {
+		w.WriteHeader(400)
+		w.Write([]byte(err.Error()))
+	}
+
+	data, err := json.Marshal(article)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(data)
+}
+
 func main() {
-    r := chi.NewRouter()
+	r := chi.NewRouter()
 
-    r.Use(middleware.Logger)
-    
-		r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-        w.Write([]byte("Hello World!"))
-    })
+	r.Use(middleware.Logger)
 
-		r.Get("/articles/{date}-{slug}", getArticle)
-    
-		http.ListenAndServe(":3000", r)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World!"))
+	})
+
+	r.Get("/articles/{date}-{slug}", getArticle)
+	r.Post("/article", createArticle)
+
+	err := http.ListenAndServe(":3000", r)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Server berlajan di port :3000")
 }
